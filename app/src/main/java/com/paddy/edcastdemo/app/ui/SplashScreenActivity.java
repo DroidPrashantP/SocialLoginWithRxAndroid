eeac1f522abaca8ee0dfb4d405521e6106ade723
Prashant Patil <prashant.patil@zingoy.com>
2017-06-09 13:32:41 +0530

    splash screen changes

--- app/src/main/java/com/paddy/edcastdemo/app/ui/SplashScreenActivity.java
+++ app/src/main/java/com/paddy/edcastdemo/app/ui/SplashScreenActivity.java
@@ -3,29 +3,55 @@ package com.paddy.edcastdemo.app.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import com.paddy.edcastdemo.app.R;
 import com.paddy.edcastdemo.app.utils.UserSharePreference;
 
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import timber.log.Timber;
+
+
 public class SplashScreenActivity extends AppCompatActivity {
-    private final static long MILLISECOND = 700;
+    private static final String TAG = "SplashScreenActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        new Handler().postDelayed(new Runnable() {
+
+        Observable observable = Observable.timer(400, TimeUnit.MILLISECONDS).observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io());
+        observable.subscribe(new Observer<Long>() {
             @Override
-            public void run() {
+            public void onError(Throwable e) {
+                Timber.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Long s) {
                 if (new UserSharePreference(SplashScreenActivity.this).isLoggedIn()) {
                     moveToNextActivity(SplashScreenActivity.this, HomeScreenActivity.class);
                 } else {
                     moveToNextActivity(SplashScreenActivity.this, SocialLoginActivity.class);
                 }
             }
-        }, MILLISECOND);
+        });
     }
 
     /**
@@ -38,4 +64,5 @@ public class SplashScreenActivity extends AppCompatActivity {
         startActivity(new Intent(activity, target));
         finish();
     }
+
 }
